f1b289ba2ede9108212093f05c7c24eb25c8d186
Erkan Zafer Dolgun <erkan@Erkans-MacBook-Air.local>
2023-11-11 05:19:42 +0300

    OrderMail&basket

---
+++ Inveon.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Inveon.Services.ProductAPI.Dto;
+using Inveon.Services.ProductAPI.Models;
+
+
+namespace Inveon.Services.ProductAPI
+{
+    public class MappingConfig : Profile
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
